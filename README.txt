c34f7dbc11c8e9db8c29a3272d9bbff100ff38b6
Beekeyy <marty-fisher@mail.ru>
2022-10-21 20:40:26 +0300

    new_py

--- README.txt
+++ README.txt
@@ -1,1 +1,7 @@
 creating vulnerability-scanner
+
+using librarries:
+1) requets
+2) re
+3) urlparse
+4) BeautifulSoup
---
+++ scanner.py
@@ -0,0 +1,35 @@
+import requests
+import re
+import urlparse
+from BeautifulSoup import BeautifulSoup
+
+class Scanner:
+    def __init__(self, url, ignore_links):
+        pass
+
+    def extract_links_form(self, url):
+        pass
+
+    def crawl(self, url = None):
+        pass
+
+    def extract_forms(self, url):
+        response = self.session.get(url)
+        parsed_html = BeautifulSoup(response.content)
+        return parsed_html.findAll("form")
+
+    def submit_for(self, form, value, url):
+        action = form.get("action")
+        post_url = urlparse.urljoin(url, action)
+        method = form.get("method")
+
+        inputs_list = form.findAll("input")
+        post_data = {}
+        for input in inputs_list:
+            input_name = input.get("name")
+            input_type = input.get("type")
+            input_value = input.get("value")
+            if input_type == "text":
+                input_value = "test"
+
+            post_data
